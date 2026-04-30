fd9640382fbf41a5761a30935a168717e53fae5d
Aleksandr Loktionov <loktionov129@gmail.com>
2026-04-30 13:14:42 +0700

    Update index.tsx

--- src/features/history/index.tsx
+++ src/features/history/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Calendar, Trash2 } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 import useWorkoutStore from '../../store/useWorkoutStore';
